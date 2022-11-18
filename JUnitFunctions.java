7ba69eaa81f8240d87389a8682cb5557e0110f12
SPYxWIZ <92371167+SPYxWIZ@users.noreply.github.com>
2022-11-18 16:34:12 +0530

    Add files via upload

---
+++ JUnitFunctions.java
@@ -0,0 +1,12 @@
+package JUnitTestPackage;
+
+public class JUnitFunctions {
+	public int AddNumbers(int a,int b)
+	{
+		return(a+b);
+	}
+	public String AddStrings(String a,String b)
+	{
+		return(a+b);
+	}
+}
